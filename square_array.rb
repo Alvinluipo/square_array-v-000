3182c3d774895eb1371e8ed17a2dff4039aad1a1
ALVIN LU <lualvin1234@gmail.com>
2019-04-04 20:13:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,17 @@
 def square_array(array)
   new_array = []
-  array.each{|num| num*2}
-  new_array << num
+  array.each do |num|
+    new_array << num*num
+  end
+  new_array
+
+end
+
+
+def square_array(array)
+  new_array=[]
+  array.each do |number|
+    new_array.push(number*number)
+    end
+    return new_array# your code here
 end
