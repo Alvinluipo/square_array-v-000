d9a273377ccfe65bbb0c3f9ef014b46ac6844950
ALVIN LU <lualvin1234@gmail.com>
2019-04-04 20:13:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,12 +6,3 @@ def square_array(array)
   new_array
 
 end
-
-
-def square_array(array)
-  new_array=[]
-  array.each do |number|
-    new_array.push(number*number)
-    end
-    return new_array# your code here
-end
